9e887ff617c13b168c83d110af5fdb1a57264762
Signal <tenzcreator1@gmail.com>
2020-06-21 22:34:17 +0200

    Empty

--- discordrebel/discordrebel.py
+++ discordrebel/discordrebel.py
@@ -5,7 +5,6 @@ import json
 import random
 from .common import Common
 
-token = 'NzIxNDk0MTA5MDg0NDUwODY4.Xu-riQ.KdyxbO4n_Q7P3d4BFAmHW4wGGYg'
 class rebelclient():
     def __init__(self, token):
         self.token = token
