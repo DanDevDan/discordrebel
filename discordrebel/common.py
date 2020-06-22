754b3c84eaf02db25f91c9f2d442fdaa0db4299d
Signal <tenzcreator1@gmail.com>
2020-06-22 12:54:08 +0200

    Converted all common methods to functions. Renamed RebelClient class to follow PEP-8. Removed unnecessary prints.

--- discordrebel/common.py
+++ discordrebel/common.py
@@ -9,68 +9,64 @@ def setup_header(token):
     headers = {
         'Authorization': token,
         'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 '
+                      'Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36 '
     }
     return headers
 
 
-class Common():
+def set_nickname(token, guildid, nickname):
+    payload = {'nick':nickname}
+    src = requests.patch(f'https://canary.discordapp.com/api/v6/guilds/{guildid}/members/@me/nick', headers=setup_header(token),
+                        json=payload, timeout=10)
 
-    def set_game(self, token, game, type, status='online', twitchlink='twitch.com'):
-        if status == "random":
-            stat = ['online', 'dnd', 'idle']
-            status = random.choice(stat)
-    
-        ws = websocket.WebSocket()
-        ws.connect("wss://gateway.discord.gg/?v=6&encoding=json")
-        print('token: ' + token)
-        print('game: ' + game)
-        print('type: ' + type)
-        print('status: ' + status)
-        if type == "Playing":
-            gamejson = {
-                "name": game,
-                "type": 0
-            }
-        elif type == 'Streaming':
-            gamejson = {
-                "name": game,
-                "type": 1,
-                "url": twitchlink
-            }
-        elif type == "Listening to":
-            gamejson = {
-                "name": game,
-                "type": 2
-            }
-        elif type == "Watching":
-            gamejson = {
-                "name": game,
-                "type": 3
-            }
-        auth = {
-            "op": 2,
-            "d": {
-                "token": token,
-                "properties": {
-                    "$os": sys.platform,
-                    "$browser": "DiscordRebel.py",
-                    "$device": f"{sys.platform} Device"
-                },
-                "presence": {
-                    "game": gamejson,
-                    "status": status,
-                    "since": 0,
-                    "afk": False
-                }
+
+def set_game(token, game, type, status='online', twitchlink='twitch.com'):
+    if status == "random":
+        stat = ['online', 'dnd', 'idle']
+        status = random.choice(stat)
+
+    ws = websocket.WebSocket()
+    ws.connect("wss://gateway.discord.gg/?v=6&encoding=json")
+    if type == "Playing":
+        gamejson = {
+            "name": game,
+            "type": 0
+        }
+    elif type == 'Streaming':
+        gamejson = {
+            "name": game,
+            "type": 1,
+            "url": twitchlink
+        }
+    elif type == "Listening to":
+        gamejson = {
+            "name": game,
+            "type": 2
+        }
+    elif type == "Watching":
+        gamejson = {
+            "name": game,
+            "type": 3
+        }
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": sys.platform,
+                "$browser": "DiscordRebel.py",
+                "$device": f"{sys.platform} Device"
             },
-            "s": None,
-            "t": None
+            "presence": {
+                "game": gamejson,
+                "status": status,
+                "since": 0,
+                "afk": False
             }
+        },
+        "s": None,
+        "t": None
+        }
 
-        ws.send(json.dumps(auth))
-
-    def set_nickname(self, token, guildid, nickname):
-        payload = {'nick':nickname}
-        src = requests.patch(f'https://canary.discordapp.com/api/v6/guilds/{guildid}/members/@me/nick', headers=setup_header(token),
-                            json=payload, timeout=10)
+    ws.send(json.dumps(auth))
--- discordrebel/discordrebel.py
+++ discordrebel/discordrebel.py
@@ -3,14 +3,15 @@ import websocket
 import sys
 import json
 import random
-from .common import Common
+from . import common
 
-class rebelclient():
+
+class RebelClient:
     def __init__(self, token):
         self.token = token
 
     def set_game(self, game, type, status='online', twitchlink='twitch.com'):
-        Common.set_game(self, self.token, game, type, status)
+        common.set_game(self.token, game, type, status, twitchlink)
 
     def set_nickname(self, token, guildid, nickname):
-        Common.set_nickname(self, self.token, guildid, nickname)
+        common.set_nickname(self.token, guildid, nickname)
